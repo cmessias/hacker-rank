dd1f88f9b2f557ac2d0de7b22ca47e5b758053f7
Caio Messias <caio.messias@pm.me>
2021-04-17 19:30:24 -0300

    Add anagram detection with frequency maps

---
+++ strings/is_anagram_map.exs
@@ -0,0 +1,24 @@
+# https://www.codewars.com/kata/529eef7a9194e0cbc1000255
+
+defmodule Anagram do
+  def anagram?(a, b) do
+    a_freq = char_frequencies(a)
+    b_freq = char_frequencies(b)
+
+    a_freq === b_freq
+  end
+
+  defp char_frequencies(string) do
+    string
+    |> String.downcase()
+    |> String.graphemes()
+    |> frequencies()
+  end
+
+  defp frequencies(list) do
+    list
+    |> Enum.reduce(%{}, fn (elem, count) ->
+         Map.update(count, elem, 1, &(&1 + 1))
+       end)
+  end
+end
