8c102e74f80b5aface8db3c5bb12584deec09368
Caio Messias <caio.messias@pm.me>
2021-04-18 10:58:07 -0300

    Remove unnecessary variables

--- strings/is_anagram_map.exs
+++ strings/is_anagram_map.exs
@@ -2,10 +2,7 @@
 
 defmodule Anagram do
   def anagram?(a, b) do
-    a_freq = char_frequencies(a)
-    b_freq = char_frequencies(b)
-
-    a_freq === b_freq
+    char_frequencies(a) === char_frequencies(b)
   end
 
   defp char_frequencies(string) do
@@ -16,9 +13,8 @@ defmodule Anagram do
   end
 
   defp frequencies(list) do
-    list
-    |> Enum.reduce(%{}, fn (elem, acc) ->
-         Map.update(acc, elem, 1, &(&1 + 1))
-       end)
+    Enum.reduce(list, %{}, fn (elem, acc) ->
+      Map.update(acc, elem, 1, &(&1 + 1))
+    end)
   end
 end
