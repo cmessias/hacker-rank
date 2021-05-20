d339d5fd0f926284f46b9327619921ec795ecd8c
Caio Messias <caio.messias@pm.me>
2021-05-20 17:47:53 -0300

    Code golf, replace fold with filter

--- strings/alternating_characters/src/main.rs
+++ strings/alternating_characters/src/main.rs
@@ -1,10 +1,11 @@
+// https://www.hackerrank.com/challenges/alternating-characters/
+// code golf, fewest lines wins
+
 use std::io;
 use std::io::BufRead;
 
-fn deletions_required(s: &str) -> u32 {
-    s.as_bytes().windows(2).fold(0, |acc, window| {
-        if window[0] == window[1] { acc + 1 } else { acc }
-    })
+fn deletions_required(s: &str) -> usize {
+    s.as_bytes().windows(2).filter(|w| return w[0] == w[1]).count()
 }
 
 fn main() {
@@ -12,12 +13,8 @@ fn main() {
     io::stdin().read_line(&mut input).unwrap();
     let n = input.trim().parse().unwrap();
 
-    let strings: Vec<String> = io::stdin()
-        .lock()
-        .lines()
-        .take(n)
-        .map(|s| s.unwrap())
-        .collect();
+    let strings: Vec<String> = io::stdin().lock().lines()
+        .take(n).map(|s| s.unwrap()).collect();
 
     for string in strings {
         println!("{}", deletions_required(&string))
