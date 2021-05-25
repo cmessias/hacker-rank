1f180fcef6f0390e097a10c6586bdfcb84ccb3a6
Caio Messias <caio.messias@pm.me>
2021-05-25 18:12:22 -0300

    code golf

--- trees/height_tree.cpp
+++ trees/height_tree.cpp
@@ -1,75 +1,12 @@
-// https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/
-// Tree declaration and IO provided by Hacker Rank
-
-#include <bits/stdc++.h>
-
-using namespace std;
-
+/* The tree node has data, left child and right child 
 class Node {
-    public:
-        int data;
-        Node *left;
-        Node *right;
-        Node(int d) {
-            data = d;
-            left = NULL;
-            right = NULL;
-        }
-};
-
-class Solution {
-    public:
-  	Node* insert(Node* root, int data) {
-            if(root == NULL) {
-                return new Node(data);
-            } else {
-                Node* cur;
-                if(data <= root->data) {
-                    cur = insert(root->left, data);
-                    root->left = cur;
-                } else {
-                    cur = insert(root->right, data);
-                    root->right = cur;
-                }
-
-               return root;
-           }
-        }
-
-        int height(Node* root) {
-            return getHeight(root, 0);
-        }
-    
-        int getHeight(Node* node, int height) {
-            if (node == nullptr) {
-                return height -1;
-            } else {
-                return max(
-                    getHeight(node->left, height + 1), 
-                    getHeight(node->right, height + 1)
-                );
-            }
-        }
-
-};
-
-int main() {
-    
-    Solution myTree;
-    Node* root = NULL;
-    
-    int t;
     int data;
-
-    std::cin >> t;
-
-    while(t-- > 0) {
-        std::cin >> data;
-        root = myTree.insert(root, data);
-    }
-  
-    int height = myTree.height(root);
-    std::cout << height;
-
-    return 0;
+    Node* left;
+    Node* right;
+};
+*/
+    
+int height(Node* node, int h = 0) {
+    if (node == nullptr) return -1;
+    return 1 + max(height(node->left, h + 1), height(node->right, h + 1));
 }
