638757da107a872e99acb05c41fa7ae3506dc2ed
Caio Messias <caio.messias@pm.me>
2021-05-27 00:05:44 -0300

    Add components-graph challenge

---
+++ graphs/components-graph/src/main.rs
@@ -0,0 +1,59 @@
+use std::cmp;
+use std::collections::HashMap;
+use std::io::BufRead;
+
+fn make_set(n: usize) -> Vec<usize> {
+    (0..=2 * n).collect()
+}
+
+fn find(x: usize, parent: &[usize]) -> usize {
+    return if x == parent[x] { x }
+        else { find(parent[x], parent) };
+}
+
+fn union(x: usize, y: usize, parent: &mut [usize]) {
+    let x = find(x, parent);
+    let y = find(y, parent);
+
+    if x == y { return; }
+    parent[y] = x;
+}
+
+fn count_components(parent: &[usize]) -> (u32, u32) {
+    let mut components = HashMap::<usize, u32>::new();
+    for &x in parent {
+        *components.entry(find(x, parent)).or_insert(0) += 1;
+    }
+
+    return components
+        .iter()
+        .filter(|(a, b)| **b > 1)
+        .fold((parent.len() as u32, 0), |(min, max), (key, value)| {
+            (cmp::min(min, *value), cmp::max(max, *value))
+        });
+}
+
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let n: usize = input.trim().parse().unwrap();
+
+    let connections: Vec<(usize, usize)> = std::io::stdin()
+        .lock()
+        .lines()
+        .take(n)
+        .map(|s| split_pair(&s.unwrap()))
+        .collect();
+
+    let mut parent = make_set(n);
+    for (x, y) in connections {
+        union(x, y, &mut parent)
+    }
+}
+
+fn split_pair(s: &str) -> (usize, usize) {
+    let mut iter = s.split_whitespace().into_iter();
+    let n1 = iter.next().unwrap().parse().unwrap();
+    let n2 = iter.next().unwrap().parse().unwrap();
+    return (n1, n2);
+}
